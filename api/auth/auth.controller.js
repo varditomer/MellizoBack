7109dbd789420bebdc928dd48ddaa2cb83f2d944
NadavSchne <nadav2502@gmail.com>
2023-12-20 18:05:24 +0200

    google calender api

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -2,7 +2,6 @@ const authService = require('./auth.service')
 
 const login = async (req, res) => {
     const userCredentials = req.body
-    console.log(userCredentials, "auth c")
     try {
         console.log(`login in:`, userCredentials)
         const user = await authService.login(userCredentials)
@@ -18,10 +17,8 @@ const login = async (req, res) => {
 const signup = async (req, res) => {
     const userDetails = req.body
     const userCredentials = { email: userDetails.email, password: userDetails.password }
-
     try {
         await authService.signup(userDetails)
-        console.log(`llllll:`,)
         const user = await authService.login(userCredentials)
         res.json(user)
     } catch (err) {
